2e3727234441b1999d273aa8af23d066fdd641da
DanilWork <bogdanovdanila1404@gmail.com>
2024-03-15 13:56:44 +0500

    chore: install drawer layout

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import TabNavigator from 'navigation/TabNavigator';
