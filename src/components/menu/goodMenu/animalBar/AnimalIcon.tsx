8b722f7d56744d9ca3314e670772eccee3f47554
DanilWork <bogdanovdanila1404@gmail.com>
2024-03-19 14:38:12 +0500

    feat: add AnimalIcon

---
+++ src/components/menu/goodMenu/animalBar/AnimalIcon.tsx
@@ -0,0 +1,37 @@
+import {Pressable, View, Text, StyleSheet} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+type AnimalIconProps = {
+  name: string;
+  title: string;
+  active: boolean;
+  onPress: () => void;
+};
+
+const AnimalIcon = ({name, title, active, onPress}: AnimalIconProps) => {
+  return (
+    <Pressable onPress={onPress}>
+      <View style={[styles.container, active && styles.active]}>
+        <MaterialCommunityIcons
+          name={name}
+          size={30}
+          color={active ? '#4F8EF7' : '#c9c9c9'}
+        />
+        <Text style={[styles.text, active && styles.active]}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    minWidth: 60,
+    padding: 5,
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  text: {color: '#c9c9c9'},
+  active: {color: '#4F8EF7', backgroundColor: '#f0f6ff'},
+});
+
+export default AnimalIcon;
