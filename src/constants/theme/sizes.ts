5b2969ac3734ba86500328d44e5422f0ebcd87bb
DanilWork <bogdanovdanila1404@gmail.com>
2024-04-25 13:25:13 +0500

    feat: add sizes constant

---
+++ src/constants/theme/sizes.ts
@@ -0,0 +1,21 @@
+const base = 8;
+
+export const SIZES = {
+  text: 14,
+
+  h1: 28,
+  h2: 24,
+  h3: 20,
+  h4: 18,
+  h5: 16,
+  h6: 13,
+
+  xs: base * 0.5, // xs: 4px
+  s: base * 1, //    s:  8px
+  sm: base * 2, //   sm: 16px
+  m: base * 3, //    m:  24px
+  md: base * 4, //   md: 32px
+  l: base * 5, //    l:  40px
+  xl: base * 6, //   xl: 48px
+  xxl: base * 7, //  xxl:56px
+};
