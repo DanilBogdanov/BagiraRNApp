990de945949f59add1bbe901cc830f7f7a9b9086
DanilWork <bogdanovdanila1404@gmail.com>
2024-04-26 14:54:28 +0500

    refactor: organize imports

--- src/components/actions/cartCountAction/CartCountAction.tsx
+++ src/components/actions/cartCountAction/CartCountAction.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
-import {COLORS, SIZES} from 'constants/theme';
 import CartCountButton from './CartCountButton';
+import {COLORS, SIZES} from 'constants/theme';
 
 type CartCountActionProps = {
   cartCount: number;
